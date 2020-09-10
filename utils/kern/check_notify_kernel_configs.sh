9726cabbd986fdfc3c03b42d0c0de6d371bf4aa9
ucgw <ucgwest@gmail.com>
2020-09-10 11:28:21 -0400

    .config file checks

--- utils/kern/check_notify_kernel_configs.sh
+++ utils/kern/check_notify_kernel_configs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 source `pwd`/dinterbuild.env 2>/dev/null || exit 1
 
-echo "====> $KERNEL_DIR/$KERNEL_CFG"
-grep -iP 'config_.*notify' "$KERNEL_DIR/$KERNEL_CFG"
+if [ -e $KERNEL_DIR/$KERNEL_CFG ];then
+  echo "====> $KERNEL_DIR/$KERNEL_CFG"
+  grep -iP 'config_.*notify' "$KERNEL_DIR/$KERNEL_CFG"
+fi
