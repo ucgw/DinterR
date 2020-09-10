a3e512d7f7ffa08d53b8f99e61478dcd36fe186e
ucgw <ucgwest@gmail.com>
2020-09-10 11:31:13 -0400

    add non-zero exit

--- utils/kern/check_notify_kernel_configs.sh
+++ utils/kern/check_notify_kernel_configs.sh
@@ -4,4 +4,6 @@ source `pwd`/dinterbuild.env 2>/dev/null || exit 1
 if [ -e $KERNEL_DIR/$KERNEL_CFG ];then
   echo "====> $KERNEL_DIR/$KERNEL_CFG"
   grep -iP 'config_.*notify' "$KERNEL_DIR/$KERNEL_CFG"
+else
+  exit 1
 fi
