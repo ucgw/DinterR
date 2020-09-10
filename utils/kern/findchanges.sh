a62ee300ebc781d5d205385dacf35d2656140efb
ucgw <ucgwest@gmail.com>
2020-09-10 11:34:20 -0400

    stderr to dev null on find op

--- utils/kern/findchanges.sh
+++ utils/kern/findchanges.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source `pwd`/dinterbuild.env 2>/dev/null || exit 1
 
-for SRCFILE in `find $KERNEL_DIR -name "*.orig"`;do
+for SRCFILE in `find $KERNEL_DIR -name "*.orig" 2>/dev/null`;do
   echo $SRCFILE | sed -e 's/\.orig//g'
 done
