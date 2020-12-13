0356f14fde1b8fc79b7779120b932ab44918dde2
ucgw <ucgwest@gmail.com>
2020-12-12 21:46:02 -0500

    initial framework for lkm

---
+++ src/lkm/DinterR_mod.c
@@ -0,0 +1,26 @@
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_DESCRIPTION("DinterR: Data interaction Reporting LKM");
+MODULE_AUTHOR("Gabe West <gawest@noctrl.edu>");
+MODULE_LICENSE("Proprietary");
+
+static const struct file_operations fops = {
+    .owner = THIS_MODULE
+};
+
+static int dinterr_init(void)
+{
+        pr_info("init\n");
+        return 0;
+}
+
+static void dinterr_exit(void)
+{
+        pr_info("exit\n");
+}
+
+module_init(dinterr_init);
+module_exit(dinterr_exit);
---
+++ src/lkm/build_mod.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+source ../../utils/kern/dinterbuild.env || exit 1
+cat << EOF > Makefile
+obj-m += DinterR_mod.o
+
+kbuild:
+	make -C $KERNEL_DIR M=`pwd`
+
+clean:
+	make -C $KERNEL_DIR M=`pwd` clean
+EOF
+make kbuild
