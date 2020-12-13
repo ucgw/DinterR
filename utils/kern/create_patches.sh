35445280b07c045c94bcfc4e614be22e6dfa3d4a
ucgw <ucgwest@gmail.com>
2020-12-13 00:43:55 -0600

    add create_patches.sh automation

---
+++ utils/kern/create_patches.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+source `pwd`/dinterbuild.env 2>/dev/null || exit 1
+dt_snap=`date +"%Y%m%d%H%M%S"`
+
+for ORIGFILE in `find $KERNEL_DIR -name "*.orig" 2>/dev/null`;do
+  SRCFILE=`echo $ORIGFILE | sed -e 's/\.orig//g'`
+  BASENAME_SRCFILE=`basename $SRCFILE`
+  PATCHFILE="$BASENAME_SRCFILE-$dt_snap.patch"
+  diff_output=`diff -u $ORIGFILE $SRCFILE`
+  if [ "$?" == "1" ];then
+      echo "$diff_output" > ../../src/kern/patches/$PATCHFILE
+  fi
+done
