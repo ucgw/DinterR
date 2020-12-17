c1ebf995e1d8833cf758313829ec95c36620bb23
ucgw <ucgwest@gmail.com>
2020-12-17 15:35:51 -0600

    make findchanges.sh order by mtime

--- utils/kern/findchanges.sh
+++ utils/kern/findchanges.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 source `pwd`/dinterbuild.env 2>/dev/null || exit 1
+changed_files=""
+crafted_find="find $KERNEL_DIR"
 
-for SRCFILE in `find $KERNEL_DIR -name "*.orig" 2>/dev/null`;do
-  echo $SRCFILE | sed -e 's/\.orig//g'
+for SRCFILE in `$crafted_find -name "*.orig" 2>/dev/null`;do
+  FILENAME=`echo $SRCFILE | sed -e 's/\.orig//g'`
+  changed_files+=" $FILENAME"
 done
+
+ls -t $changed_files
