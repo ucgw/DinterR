648096f56de491a9e9782b89ad515ca62242dbea
ucgw <ucgwest@gmail.com>
2020-12-20 20:29:57 -0600

    inotify c code taken from man page

---
+++ src/user/include/dinterr.h
@@ -0,0 +1,9 @@
+#ifndef _DINTERR_H_
+#define _DINTERR_H_
+#include <unistd.h>
+
+/* DinterR extra data */
+typedef size_t    dinterr_count_t;
+typedef long long dinterr_pos_t;
+
+#endif  /* _DINTERR_H_ */
