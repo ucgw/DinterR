13f5b0392387fc1130ec6a38f9a48fea307b3c74
ucgw <ucgwest@gmail.com>
2020-12-25 01:38:03 -0600

    partial fix: need to update size_t to ssize_t in vfs_read() for negative seeks

--- src/user/dinterrd/randseek.py
+++ src/user/dinterrd/randseek.py
@@ -4,17 +4,28 @@ import random
 randpos1 = random.randint(10,20);
 randpos2 = random.randint(10,20);
 randpos3 = random.randint(10,20);
+randpos4 = random.randint(10,20);
 
 with open('Makefile', 'r') as fh:
     print("Random Pos (from start): {0}".format(randpos3))
     fh.seek(randpos3, 0)
     print(fh.tell())
     print(fh.readline().decode('utf-8'))
+
+with open('Makefile', 'r') as fh:
     print("Random Pos (from end): {0}".format(-randpos1))
     fh.seek(-randpos1, 2)
     print(fh.tell())
     print(fh.readline().decode('utf-8'))
+
+with open('Makefile', 'r') as fh:
     print("Random Pos (from start): {0}".format(randpos2))
     fh.seek(randpos2, 0)
+    print(fh.tell())
+    print(fh.readline().decode('utf-8'))
+
+with open('Makefile', 'r') as fh:
+    print("Random Pos (from end): {0}".format(-randpos4))
+    fh.seek(-randpos4, 2)
     print(fh.tell())
     print(fh.readline().decode('utf-8'))
