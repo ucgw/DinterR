a68865f4c13012b88dbf72892efa0d905432b36a
ucgw <ucgwest@gmail.com>
2021-01-01 23:37:43 -0600

    investigate: start migrating towards boost.sml

--- src/user/dinterrd/common/include/netproto.h
+++ src/user/dinterrd/common/include/netproto.h
@@ -1,21 +1,40 @@
+/* Dinterr Data Transport (DDT) Protocol implementation:
+ * pattern and templating functionality via
+ * Boost.SML
+ *
+ * Reference:
+ * https://boost-ext.github.io/sml/index.html
+ *
+ * (requires minimum of -std=c++14 (i.e. c++ 14 standard))
+ */
+
 #ifndef _NETPROTO_H_
 #define _NETPROTO_H_
 
+#include <cassert>
+
 #include "sml.hpp"
 
-enum class DinterrTransportState {
-  INIT,
-  LOAD,
-  LOAD_ACK,
-  LOAD_FAIL,
-  CTS,
-  AGG,
-  REC,
-  DATA_RETRY,
-  DATA_FIN,
-  DATA_FINACK,
-  LOAD_FIN,
-  LOAD_FINACK
+namespace sml = boost::sml;
+
+namespace {
+/* events: (hold no data) */
+struct load_requested {};
+struct load_failed {};
+struct load_succeeded {};
+struct record_data_ready_to_send {};
+struct aggregate_data_ready_to_send {};
+struct record_data_received {};
+struct record_data_retry_requested {};
+struct record_data_finished {};
+struct load_finish_requested {};
+struct load_finished {};
+
+struct ddt_transitions {
+    auto operator()() const noexcept {
+    using namespace sml;
+    }
 };
+} // namespace
 
 #endif  // _NETPROTO_H_
--- src/user/dinterrd/common/netproto.cpp
+++ src/user/dinterrd/common/netproto.cpp
@@ -1,2 +1,11 @@
+/* Dinterr Data Transport (DDT) Protocol implementation:
+ * pattern and wrapper functionality via
+ * Boost.SML
+ *
+ * Reference:
+ * https://boost-ext.github.io/sml/index.html
+ *
+ * (requires minimum of -std=c++14 (i.e. c++ 14 standard))
+ */
+
 #include "netproto.h"
-namespace sml = boost::sml;
