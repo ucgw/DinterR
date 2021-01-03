d775a54518fbdbc18dbea50c324d75cb6326cf1a
ucgw <ucgwest@gmail.com>
2021-01-02 22:35:48 -0600

    initial fsm for client and server protocol

--- src/user/dinterrd/common/include/netproto.h
+++ src/user/dinterrd/common/include/netproto.h
@@ -21,29 +21,63 @@ namespace {
 /* events */
 struct load_request {};
 struct load_fail {};
-struct load_succeed {};
+struct load_success {};
 struct data_send {};
-struct data_receive {};
+struct data_receive {};  // client internal
 struct data_retry_request {};
 struct data_verify_confirm {};
-struct data_process {};
-struct data_purge {};
+struct data_process {};  // client internal
+struct data_purge {};    // server internal
 struct unload_request {};
 struct unload_complete {};
+struct terminate {};
 
 /* state machine for the DDTP server */
-struct ddt_server {
+struct ddtp_server {
     auto operator()() const {
-    using namespace sml;
+        using namespace sml;
+        return make_transition_table(
+          *"load_wait"_s   + event<load_fail> = "load_wait"_s,
+           "load_wait"_s   + event<load_success> = "data_ready"_s,
+           "data_ready"_s  + event<data_send> = "data_pend"_s,
+           "data_ready"_s  + event<unload_request> = "unload_pend"_s,
+           "data_pend"_s   + event<data_verify_confirm> = "data_purge"_s,
+           "data_pend"_s   + event<data_retry_request> = "data_ready"_s,
+           "data_purge"_s  + event<data_purge> = "data_ready"_s,
+           "unload_pend"_s + event<unload_complete> = "load_wait"_s,
+           "load_wait"_s   + event<terminate> = X,
+           "data_ready"_s  + event<terminate> = X
+        );
     }
 };
 
 /* state machine for the DDTP client */
-struct ddt_client {
+struct ddtp_client {
     auto operator()() const {
-    using namespace sml;
+        using namespace sml;
+        return make_transition_table(
+          *"load_init"_s    + event<load_request> = "load_pend"_s,
+           "load_pend"_s    + event<load_fail> = X,
+           "load_pend"_s    + event<load_success> = "data_wait"_s,
+           "data_wait"_s    + event<data_receive> = "data_verify"_s,
+           "data_wait"_s    + event<unload_request> = "unload_pend"_s,
+           "data_verify"_s  + event<data_retry_request> = "data_wait"_s,
+           "data_verify"_s  + event<data_verify_confirm> = "data_process"_s,
+           "data_process"_s + event<data_process> = "data_wait"_s,
+           "unload_pend"_s  + event<unload_complete> = X
+        );
     }
 };
 } // namespace
+
+/* DDT Protocol Types (akin to certain SML events) */
+#define LOAD_REQUEST    0x1
+#define LOAD_FAIL       0x2
+#define LOAD_SUCCEED    0x3
+#define DATA_SEND       0x4
+#define DATA_RETRY      0x5
+#define DATA_CONFIRM    0x6
+#define UNLOAD_REQUEST  0x7
+#define UNLOAD_COMPLETE 0x8
 
 #endif  // _NETPROTO_H_
