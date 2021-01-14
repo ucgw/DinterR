fbce6a7463c510cd1025e23c0ee846618f5f9920
ucgw <ucgwest@gmail.com>
2021-01-14 15:11:31 -0600

    rename socket read/write declarations

--- src/user/dinterrd/common/include/sockio.h
+++ src/user/dinterrd/common/include/sockio.h
@@ -52,7 +52,7 @@ int dinterrd_connect(dinterr_sock_t*); // client
 void dinterrd_run_server(dinterr_sock_t*, uint16_t, const char* ipaddr=NULL);
 
 // basic data read / write over sockets
-int _sock_read(dinterr_sock_t*, char*, size_t);
-int _sock_write(dinterr_sock_t*, char*);
+int dinterr_sock_read(dinterr_sock_t*, char*, size_t);
+int dinterr_sock_write(dinterr_sock_t*, char*);
 
 #endif // _SOCKIO_H_
