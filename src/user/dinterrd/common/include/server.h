8f9c4283ba2a1f9f2f3460f4de3da2f0298d880c
ucgw <ucgwest@gmail.com>
2021-01-17 00:38:09 -0600

    update framework for inotify load

--- src/user/dinterrd/common/include/server.h
+++ src/user/dinterrd/common/include/server.h
@@ -1,16 +1,20 @@
 #ifndef _SERVER_H_
 #define _SERVER_H_
 
-#include <map>
 #include <iostream>
+
+#include <dinterr_inotify.h>
+#include <stdlib.h>
 
 #include "netproto.h"
 #include "serdes.h"
 #include "sockio.h"
+#include "maps.h"
 
 int dinterrd_processor_sm_wrapper(dinterr_sock_t*, char*, uint16_t);
 int _dinterrd_processor(dinterr_sock_t*, sml::sm<ddtp_server>*, char*, uint16_t);
 bool ddtp_server_validate_incoming_type(short, sml::sm<ddtp_server>*);
 void ddtp_server_process_incoming_payload(ddtp_payload_t*, sml::sm<ddtp_server>*, bool);
+int ddtp_server_load_file_inotify(ddtp_payload_t*);
 
 #endif // _SERVER_H_
