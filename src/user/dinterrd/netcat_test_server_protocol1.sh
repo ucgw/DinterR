a6f76e52edb3f64f05267927ac65881ab648c805
ucgw <ucgwest@gmail.com>
2021-01-18 22:06:49 -0600

    better comments on what test is

--- src/user/dinterrd/netcat_test_server_protocol1.sh
+++ src/user/dinterrd/netcat_test_server_protocol1.sh
@@ -13,8 +13,8 @@ port=$1
 # the remaining ones should fail validation and the
 # the dinterrd service should tranistion to the terminal
 # state.
+#
+# the data sent to the server for this test should be
+# type = 0x01 (LOAD_REQUEST)
+# data = /home/dinter/csce499/DinterR/src/user/dinterrd/bigfile
 echo -n -e "\x01\x00\x2f\x68\x6f\x6d\x65\x2f\x64\x69\x6e\x74\x65\x72\x2f\x63\x73\x63\x65\x34\x39\x39\x2f\x44\x69\x6e\x74\x65\x72\x52\x2f\x73\x72\x63\x2f\x75\x73\x65\x72\x2f\x64\x69\x6e\x74\x65\x72\x72\x64\x2f\x62\x69\x67\x66\x69\x6c\x65" | nc localhost $port
-#echo -n -e "\x10" | nc localhost $port
-#echo -n -e "\x20" | nc localhost $port
-#echo -n -e "\x40" | nc localhost $port
-#echo -n -e "\xa1" | nc localhost $port
