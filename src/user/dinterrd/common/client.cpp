b187f425f3149ad00ba814428433108ef6374d10
ucgw <ucgwest@gmail.com>
2021-01-18 21:56:22 -0600

    fix netcat for testing load_request

--- src/user/dinterrd/common/client.cpp
+++ src/user/dinterrd/common/client.cpp
@@ -14,8 +14,8 @@ int main(int argc, char** argv) {
     ddtp_payload_t* pl2;
 
     ddtp_payload_init(LOAD_REQUEST, &pl);
-    //ddtp_payload_fill_char_data(&pl, fpath.c_str());
-    ddtp_payload_fill_ulong_data(&pl, crc);
+    ddtp_payload_fill_char_data(&pl, fpath.c_str());
+    //ddtp_payload_fill_ulong_data(&pl, crc);
 
     DinterrSerdesNetwork* cli_pl = new DinterrSerdesNetwork(&pl);
     char* data = (char*) cli_pl->get_data();
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
     pl2 = (ddtp_payload_t*) cli_pl2->get_data();
     uLong pl2_ulong = ddtp_payload_extract_ulong_data(pl2);
 
-    std::cout << "Type: " << pl2->type << " Data: " << pl2_ulong << std::endl;
+    //std::cout << "Type: " << pl2->type << " Data: " << pl2_ulong << std::endl;
 
     delete cli_pl2;
 
