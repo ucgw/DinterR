8b0040e1cdf2126ccd3b4c15f1eb7b13c46e2a1a
ucgw <ucgwest@gmail.com>
2021-01-23 15:57:36 -0600

    add new threadsafe queue class header

---
+++ src/user/dinterrd/common/include/queue.h
@@ -0,0 +1,58 @@
+/*
+ * Code taken from:
+ *   https://codetrips.com/2020/07/26/modern-c-writing-a-thread-safe-queue/
+ *
+ * Date: 2021-01-23
+ */
+
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include <queue>
+#include <mutex>
+
+template<typename T>
+class ThreadsafeQueue {
+  std::queue<T> queue_;
+  mutable std::mutex mutex_;
+
+  // Moved out of public interface to prevent races between this
+  // and pop().
+  bool empty() const {
+    return queue_.empty();
+  }
+
+ public:
+  ThreadsafeQueue() = default;
+  ThreadsafeQueue(const ThreadsafeQueue<T> &) = delete ;
+  ThreadsafeQueue& operator=(const ThreadsafeQueue<T> &) = delete ;
+
+  ThreadsafeQueue(ThreadsafeQueue<T>&& other) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_ = std::move(other.queue_);
+  }
+
+  virtual ~ThreadsafeQueue() { }
+
+  unsigned long size() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.size();
+  }
+
+  std::optional<T> pop() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queue_.empty()) {
+      return {};
+    }
+    T tmp = queue_.front();
+    queue_.pop();
+    return tmp;
+  }
+
+  void push(const T &item) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(item);
+  }
+};
+
+#endif // _QUEUE_H_
