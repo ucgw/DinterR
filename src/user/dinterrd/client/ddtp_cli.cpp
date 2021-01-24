92c9268e11047b1f8ac1d2c0b48fa8a4eb5d4c26
ucgw <ucgwest@gmail.com>
2021-01-23 19:49:04 -0600

    some intial cli instructions

--- src/user/dinterrd/client/ddtp_cli.cpp
+++ src/user/dinterrd/client/ddtp_cli.cpp
@@ -1,2 +1,11 @@
+#include "netproto.h"
+#include "sockio.h"
+#include "serdes.h"
+#include "payload.h"
+#include "maps.h"
+
 int main(int argc, char** argv) {
+    using namespace sml;
+
+    sml::sm<ddtp_client> sm;
 }
