ccd936ba133dec513ca1a44a63fffdab8db69676
ucgw <ucgwest@gmail.com>
2021-02-03 08:31:47 -0600

    redirect ps stderr when no process exists

--- src/user/dinterrd/netproto_test_list_tids.sh
+++ src/user/dinterrd/netproto_test_list_tids.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ps -T -p `ps -ef |grep netproto | grep -v grep | grep -vP '\/bin\/sh' | grep -v watch | awk '{ print $2 }'`
+ps -T -p `ps -ef | grep netproto | grep -v grep | grep -vP '\/bin\/sh' | grep -v watch | awk '{ print $2 }'` 2>/dev/null
